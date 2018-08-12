b8e77682883f9688f0c7468a2591b37971f6bbd1
Nicky Liu <nickyliu91@gmail.com>
2018-08-12 10:25:03 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,3 +2,19 @@
 def badge_maker(name)
   puts "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(array)
+  array2 = []
+  array.each do |x|
+    array2 << "Hello, my name is #{x}."
+  end
+  array2
+end
+
+def assign_rooms(list)
+  array2 = []
+  list.each_with_index do |name, index|
+    array2 << "Hello, #{name}! You'll be assigned to room #{index}!"
+  end
+  array2
+end
